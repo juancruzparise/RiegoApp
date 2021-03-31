a18a1dce8b59cdb839f3031d3768ed0445d81492
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2021-03-30 21:33:55 -0300

    Cambio de diseño

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import {Icon} from "react-native-elements";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
 import LoginScreen from "./Screens/LoginScreen";
 import LogoutScreen from "./Screens/LogoutScreen";
 import PlantasScreen from "./Screens/PlantasScreen";
@@ -9,11 +12,30 @@ import RegarScreen from "./Screens/RegarScreen";
 
 const Stack = createStackNavigator();
 
+function PlantasNavigator(){
+    return(
+        <NavigationContainer independent={true}>
+                <Stack.Navigator initialRouteName="Plantas">
+                <Stack.Screen
+                         name="Plantas"
+                         component={PlantasScreen}
+                    />
+                    <Stack.Screen
+                        name="Regar"
+                        component={RegarScreen}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+  )
+} 
+
+const Tab = createBottomTabNavigator();
+
 export default class App extends Component{
 
     constructor(props){
         super(props);
-        this.state = {isLoggedIn:true}
+        this.state = {isLoggedIn:false}
     }
 
     setLogin(){
@@ -27,21 +49,28 @@ export default class App extends Component{
     render(){
         if(this.state.isLoggedIn){
             return(
-                <NavigationContainer independent={true}>
-                    <Stack.Navigator initialRouteName="Plantas">
-                    <Stack.Screen
+                <NavigationContainer>
+                    <Tab.Navigator initialRouteName="Plantas">
+                    <Tab.Screen
                             name="Plantas"
-                            component={PlantasScreen}
+                            component={PlantasNavigator}
+                            options={{
+                                tabBarIcon: ({ color, size }) => (
+                                    <Icon type="foundation" name="trees" color={color} size={size} />
+                                    ),
+                            }}
                         />
-                        <Stack.Screen
-                            name="Regar"
-                            component={RegarScreen}
-                        />
-                        <Stack.Screen
-                            name="Logout"
+                        <Tab.Screen
+                            name="Cerrar Sesión"
                             component={LogoutScreen}
+                            onLogout={() => this.setLogout()}
+                            options={{
+                                tabBarIcon: ({ color, size }) => (
+                                    <Icon type="antdesign" name="logout" color={color} size={size} />
+                                    ),
+                            }}
                         />
-                    </Stack.Navigator>
+                    </Tab.Navigator>
                 </NavigationContainer>
             );
         } else {
