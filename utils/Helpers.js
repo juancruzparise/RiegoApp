95249aff7b275fbf09c4150e89f0d85b487f1ed2
juancruzparise <63892409+juancruzparise@users.noreply.github.com>
2021-04-03 21:38:47 -0300

    RiegoApp3.5

--- utils/Helpers.js
+++ utils/Helpers.js
@@ -2,17 +2,27 @@ import Firebase from './Firebase'
 
 class Helpers{
 
-    static getRiego(){
+    static setRiego(riego){
+
+
+    }
+    static getRiego(callback){
         Firebase.database().ref('/planta/riego').once('value').then(riego => {
             console.log('Riego: ', riego.val());
-            /*let riegoPlanta = ''
+            let riegoPlanta = ''
+            let tempPlanta = ''
+            let humePlanta = ''
             if(riego.val()){
                 riegoPlanta = riego.val()
             }
-            callback(riegoPlanta)*/
-            return (riego.val());
+            if(temp.val()){
+                tempPlanta = temp.val()
+            }
+            if(humedad.val()){
+                humePlanta = humedad.val()
+            }
+            callback(riegoPlanta, tempPlanta, humePlanta)
           });
     }
 }
-
-module.exports = Helpers;
+ module.exports = Helpers;
